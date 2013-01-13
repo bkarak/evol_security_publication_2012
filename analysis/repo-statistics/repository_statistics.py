a99261e099bb55f5a6f319724f3d8cdd234c1d17
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-01-13 18:50:21 +0200

    added statistics.py; exported basic measurements for version

--- analysis/repo-statistics/repository_statistics.py
+++ analysis/repo-statistics/repository_statistics.py
@@ -1,5 +1,7 @@
 from helpers.data_helper import load_projects_json
 
+from maths import statistics
+
 __author__ = 'Vassilios Karakoidas (vassilios.karakoidas@gmail.com)'
 
 
@@ -9,14 +11,18 @@ def main():
     project_count = len(project_list)
     version_list = [x.version_count() for x in project_list]
     version_count = sum(version_list)
-    max_version = max(version_list)
-    min_version = min(version_list)
+
+    version_list = sorted(version_list)
 
     print "Projects: %d" % (project_count,)
     print "Versions (total): %d" % (version_count,)
-    print "Avg. Version/Project: %.2f" % (float(version_count)/float(project_count))
-    print "Max. Version Count: %d" % (max_version,)
-    print "Min. Version Count: %d" % (min_version,)
+    print "Max. Version Count: %d" % (statistics.stat_max(version_list),)
+    print "Min. Version Count: %d" % (statistics.stat_min(version_list),)
+    print "Mean: %.2f" % (statistics.mean(version_list))
+    print "Median: %d" % (statistics.median(version_list))
+    print "Range: %d" % (statistics.stat_range(version_list))
+    print "1st Qrt: %d" % (version_list[statistics.first_quartile(version_list)])
+    print "3rd Qrt: %d" % (version_list[statistics.third_quartile(version_list)])
 
 
 if __name__ == "__main__":
