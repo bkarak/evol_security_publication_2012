cb4d89844b6a240be717a1fccefd44c3d281bb1c
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-01-13 18:31:42 +0200

    added max, min version

--- analysis/repo-statistics/repository_statistics.py
+++ analysis/repo-statistics/repository_statistics.py
@@ -7,11 +7,16 @@ def main():
     project_list = load_projects_json()
 
     project_count = len(project_list)
-    version_count = sum([x.version_count() for x in project_list])
+    version_list = [x.version_count() for x in project_list]
+    version_count = sum(version_list)
+    max_version = max(version_list)
+    min_version = min(version_list)
 
     print "Projects: %d" % (project_count,)
     print "Versions (total): %d" % (version_count,)
-    print "Avg. version/project: %.2f" % (float(version_count)/float(project_count))
+    print "Avg. Version/Project: %.2f" % (float(version_count)/float(project_count))
+    print "Max. Version Count: %d" % (max_version,)
+    print "Min. Version Count: %d" % (min_version,)
 
 
 if __name__ == "__main__":
