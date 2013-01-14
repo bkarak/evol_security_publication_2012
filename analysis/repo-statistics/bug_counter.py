6deb6f5a055c059394660cfe5d24bf85863f959c
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-01-14 17:17:58 +0200

    added basic bug counter

--- analysis/repo-statistics/bug_counter.py
+++ analysis/repo-statistics/bug_counter.py
@@ -4,14 +4,20 @@ __author__ = 'Vassilios Karakoidas (vassilios.karakoidas@gmail.com)'
 
 
 def main():
-    miter = MongoDocumentIterator(fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version','BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
+    miter = MongoDocumentIterator(query={'BugCollection.BugInstance.category':'SECURITY'}, fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version','BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
 
     print 'Found %d Documents' % (miter.total(),)
 
     while miter.has_next():
         d = miter.next()
 
-        print d
+        bug_counter = 0
+
+        for bi in d.get('BugCollection', {}).get('BugInstance', []):
+            if bi.get('category', '') == 'SECURITY':
+                bug_counter += 1
+
+        print '%s/%s-%s.jar: %d' % (d['JarMetadata']['group_id'], d['JarMetadata']['artifact_id'], d['JarMetadata']['version'], bug_counter)
 
 if __name__ == "__main__":
     main()
