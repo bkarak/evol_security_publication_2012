20f3913f6754aea8b917c163fc20896ecf73f558
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-01-14 17:54:53 +0200

    counting security with malicious code bug together

--- analysis/repo-statistics/bug_counter.py
+++ analysis/repo-statistics/bug_counter.py
@@ -4,7 +4,7 @@ __author__ = 'Vassilios Karakoidas (vassilios.karakoidas@gmail.com)'
 
 
 def main():
-    miter = MongoDocumentIterator(query={'BugCollection.BugInstance.category':'MALICIOUS_CODE'}, fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version','BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
+    miter = MongoDocumentIterator(query={'BugCollection.BugInstance.category':'SECURITY'}, fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version','BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
 
     print 'Found %d Documents' % (miter.total(),)
 
