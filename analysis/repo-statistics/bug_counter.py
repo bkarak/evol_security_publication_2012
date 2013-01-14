bc2d6bdef353a497968c9f51f1c5372bd9da7624
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-01-14 17:50:55 +0200

    playing with bug_counter

--- analysis/repo-statistics/bug_counter.py
+++ analysis/repo-statistics/bug_counter.py
@@ -4,7 +4,7 @@ __author__ = 'Vassilios Karakoidas (vassilios.karakoidas@gmail.com)'
 
 
 def main():
-    miter = MongoDocumentIterator(query={'$or: {'BugCollection.BugInstance.category':'MALICIOUS_CODE'}, {'BugCollection.BugInstance.category':'SECURITY'}'}, fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version','BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
+    miter = MongoDocumentIterator(query={'BugCollection.BugInstance.category':'MALICIOUS_CODE'}, fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version','BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
 
     print 'Found %d Documents' % (miter.total(),)
 
@@ -23,10 +23,9 @@ def main():
                 continue
 
             if bi.get('category', '') == 'SECURITY':
-                bug_counter += 1
-                
-            if bi.get('category', '') == 'MALICIOUS_CODE':
-                bug_counter += 1
+                sec_bug_counter += 1
+            elif bi.get('category', '') == 'MALICIOUS_CODE':
+                mal_bug_counter += 1
 
         print '%s/%s-%s.jar: sec: %d mal: %d (total:%d)' % (d['JarMetadata']['group_id'], d['JarMetadata']['artifact_id'], d['JarMetadata']['version'], sec_bug_counter, mal_bug_counter, total_counter)
 
