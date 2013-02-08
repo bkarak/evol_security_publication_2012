b2b8610fc12c2141e6c5dab5490d07421ad0b147
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-02-08 23:48:20 +0200

    added online version of check_utility

--- analysis/repo-statistics/check_missing.py
+++ analysis/repo-statistics/check_missing.py
@@ -1,10 +1,20 @@
-import os
 import xmldict
+import httplib
+
+from urllib2 import urlopen
 
 from helpers.mongo_helper import get_version, get_mongo_connection, MONGO_COL
 
+def url_exists(base_url, path):
+    conn = httplib.HTTPConnection(base_url)
+    conn.request('HEAD', path)
+    response = conn.getresponse()
+    conn.close()
+    return response.status == 200
+
+
 def main():
-    base_url = '/Users/bkarak/devel/repositories/maven/maven/'
+    base_url = 'http://mirrors.ibiblio.org/maven2'
     col_obj = get_mongo_connection()[MONGO_COL]
     fp = open('data/missing_versions.txt', 'r')
 
@@ -14,32 +24,35 @@ def main():
 
     for line in fp:
         (group_id, artifact_id) = line.strip().split('||')
-        maven_base_url = '%s%s/%s/' % (base_url, group_id.replace('.', '/'), artifact_id)
+        maven_base_url = '%s/%s/%s/' % (base_url, group_id.replace('.', '/'), artifact_id)
         maven_metadata_name = '%smaven-metadata.xml' % (maven_base_url,)
 
-        if not os.path.exists(maven_metadata_name):
+        try:
+            fp = urlopen(maven_metadata_name)
+            json_xml = xmldict.parse(fp.read())
+            versions = json_xml.get('metadata', {}).get('versioning', {}).get('versions', {}).get('version')
+            version_list = []
+
+            if isinstance(versions, list):
+                version_list.extend(versions)
+            else:
+                version_list.append(versions)
+
+            for v in version_list:
+                docs = get_version(col_obj, group_id, artifact_id, v)
+                total_jars += 1
+
+                if len(docs) == 0:
+                    missing += 1
+                    print '[%d]: Missing %s||%s||%s' % (total_jars, group_id, artifact_id, v)
+                    url_jar_path = '/maven2/%s/%s/%s/%s-%s.jar' % (group_id.replace('.', '/'), artifact_id, v, artifact_id, v)
+
+                    if not url_exists('mirrors.ibiblio.org', url_jar_path):
+                        really_missing += 1
+                        print '%s%s' % ('mirrors.ibiblio.org', url_jar_path)
+        except Exception, e:
+            print '[%d] ERROR: %s (%s)' % (total_jars, maven_metadata_name, e)
             continue
-
-        json_xml = xmldict.parse(open(maven_metadata_name, 'r').read())
-        versions = json_xml.get('metadata', {}).get('versioning', {}).get('versions', {}).get('version')
-        version_list = []
-
-        if isinstance(versions, list):
-            version_list.extend(versions)
-        else:
-            version_list.append(versions)
-
-        for v in version_list:
-            docs = get_version(col_obj, group_id, artifact_id, v)
-            total_jars += 1
-
-            if len(docs) == 0:
-                missing += 1
-                print '[%d]: Missing %s||%s||%s' % (total_jars, group_id, artifact_id, v)
-                local_jar_path = '%s%s/%s-%s.jar' % (maven_base_url, v, artifact_id, v)
-
-                if not os.path.exists(local_jar_path):
-                    really_missing += 1
 
     fp.close()
 
