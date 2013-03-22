ff385cd99c74dba7229de23fe98762e78fdf5dcb
Panos Louridas <louridas@aueb.gr>
2013-03-22 17:53:37 +0200

    calculate and produce correlation matrix plot

--- analysis/repo-statistics/bug_correlation_calc.R
+++ analysis/repo-statistics/bug_correlation_calc.R
@@ -2,7 +2,24 @@
 # data/bug_correlation_counters.csv, which is produced
 # by bug_correlation_tab.py
 
+library(ellipse)
+
 bug.counts <- read.csv("data/bug_correlation_counters.csv")
+
+bug.counts$SECURITY_HIGH <- bug.counts$SECURITY_HIGH + bug.counts$MALICIOUS_CODE
+
+bug.counts$MALICIOUS_CODE <- NULL
+
+bug.counts$TOTAL_MALICIOUS_CODE <- NULL
+bug.counts$TOTAL_SECURITY_HIGH <- NULL
+bug.counts$TOTAL_SECURITY_LOW <- NULL
+bug.counts$TOTAL_STYLE <- NULL
+bug.counts$TOTAL_BAD_PRACTICE <- NULL
+bug.counts$TOTAL_CORRECTNESS <- NULL
+bug.counts$TOTAL_MT_CORRECTNESS <- NULL
+bug.counts$TOTAL_PERFORMANCE <- NULL
+bug.counts$TOTAL_I18N <- NULL
+bug.counts$TOTAL_EXPERIMENTAL <- NULL
 
 for (i in (2:(ncol(bug.counts) - 1))) {
   for (j in ((i + 1):(ncol(bug.counts) - 1))) {
@@ -10,3 +27,10 @@ for (i in (2:(ncol(bug.counts) - 1))) {
     print(cor.test(bug.counts[, i], bug.counts[, j]))
   }
 }
+
+ctab <- cor(bug.counts[,-1], method="pearson", use="pairwise.complete.obs")
+png('corrplot.png')
+colorfun <- colorRamp(c("#ff0000","white","#3366CC"), space="Lab")
+plotcorr(ctab, col=rgb(colorfun((ctab+1)/2), maxColorValue=255))
+dev.off()
+ctab
--- analysis/repo-statistics/bug_correlation_calc.py
+++ analysis/repo-statistics/bug_correlation_calc.py
@@ -1,22 +1,48 @@
 # Calculates pairwise correlations from the data in
 # data/bug_correlation_counters.json
 
-
 import json
 import pandas as pd
 import numpy as np
 import scipy.stats as st
 from itertools import izip
 import sys
+import itertools
+from corrplot import corrplot
 
+bug_types = [
+    'SECURITY_HIGH',
+    'SECURITY_LOW',
+    'STYLE',
+    'BAD_PRACTICE',
+    'CORRECTNESS',
+    'MT_CORRECTNESS',
+    'PERFORMANCE',
+    'I18N',
+    'EXPERIMENTAL'
+    ]
+    
 with open("data/bug_correlation_counters.json") as infile:
     json_input = json.load(infile)
 
 data = pd.DataFrame(json_input).T
+data['SECURITY_HIGH'] = data['SECURITY_HIGH'] + data['MALICIOUS_CODE']
+data['TOTAL_SECURITY_HIGH'] = (data['TOTAL_SECURITY_HIGH']
+                               + data['TOTAL_MALICIOUS_CODE'])
+data = data.drop(['MALICIOUS_CODE', 'TOTAL_MALICIOUS_CODE'], axis=1)
 
-for i in xrange(len(data.columns) - 1):
-    for j in xrange(i + 1, (len(data.columns) - 1)):
-        frm = data.ix[:, [i, j]].dropna()
-        (corr, pvalue) = st.pearsonr(frm.ix[:, 0], frm.ix[:, 1])
-        print data.columns[i], data.columns[j], corr, pvalue
+num_bug_types = len(bug_types)
+corrmatrix = np.identity(num_bug_types)
+pvalues = np.zeros([num_bug_types, num_bug_types])
 
+for i in xrange(num_bug_types):
+    for j in xrange(i + 1, num_bug_types):
+        icolumn = bug_types[i]
+        jcolumn = bug_types[j]
+        data_pair = data[[icolumn, jcolumn]].dropna()
+        (corr, pvalue) = st.pearsonr(data_pair[icolumn], data_pair[jcolumn])
+        print icolumn, jcolumn, corr, pvalue
+        corrmatrix[i, j] = corrmatrix[j, i] = corr
+        pvalues[i, j] = pvalues[j, i] = pvalue
+
+corrplot(corrmatrix, pvalues, bug_types)
