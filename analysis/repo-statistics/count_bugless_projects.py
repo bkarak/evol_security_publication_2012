8255561fa47c7e6069f9a413f1872c755d5c8e37
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2013-03-24 22:09:02 +0200

    updated bugless projedcts scripts

--- analysis/repo-statistics/count_bugless_projects.py
+++ analysis/repo-statistics/count_bugless_projects.py
@@ -1,23 +1,33 @@
 import json
 
+from helpers.data_helper import ArrayCount, save_to_file, load_projects_json
+from helpers.mongo_helper import MongoProjectIterator
+
 
 def main():
-    fp_two = open('data/project_versions.json', 'r')
-    project_json = json.load(fp_two)
-    fp = open('data/bugless_projects.json', 'r')
+    projects = load_projects_json()
+    total_projects = len(projects)
+    count = 0
+    bugless_count = 0
 
-    total_projects = 0
-    version_count = 0
+    print 'Found %d Projects' % (total_projects,)
 
-    for l in fp:
-        version_count += project_json.get(l.strip(), 0)
-        total_projects += 1
+    for p in projects:
+        piter = MongoProjectIterator(p.group_id(), p.artifact_id(), fields=['JarMetadata.group_id', 'JarMetadata.artifact_id', 'JarMetadata.version', 'JarMetadata.version_order', 'BugCollection.BugInstance.category', 'BugCollection.BugInstance.type'])
+        doc_list = piter.documents_list()
+        proj_array_count = ArrayCount()
+        bug_list = []
+        count += 1
 
-    fp.close()
-    fp_two.close()
+        for d in doc_list:
+        	bug_instances = d.get('BugCollection', {}).get('BugInstance', [])
+        	if len(bug_instances) == 0:
+        		bugless_count += 1
+        		break
 
-    print 'Total Projects: %d, Version: %d' % (total_projects, version_count)
+        print '[%d:%d:%d] %s||%s: %d versions' % (count, total_projects, bugless_count, p.group_id(), p.artifact_id(), len(doc_list))
 
+    print "bugless: %d, total: %d" % (bugless_count, total)
 
 if __name__ == "__main__":
     main()
